1d9620402baadd5a7911ff34b38ae9620263936b
Pratik90658 <65339936+Pratik90658@users.noreply.github.com>
2020-05-19 18:56:23 +0530

    Update script.js

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -1,6 +1,6 @@
 (function () {
    // var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-   var names = prompt("Enter Name :");
+   var names = prompt("Enter the Name And Open Console ");
    for (var name in names) {
       var firstLetter = names[name].charAt(0).toLowerCase();
        if (firstLetter === 'j') {
